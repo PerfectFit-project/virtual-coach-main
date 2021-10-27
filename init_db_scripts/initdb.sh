ab5a956447d266a25356720a9e4c6da15e8c03bc
Sven van der Burg <s.vanderburg@esciencecenter.nl>
2021-10-27 08:40:09 +0200

    Revert "Review addition of e2etests"

--- init_db_scripts/initdb.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-dump_fname=/docker-entrypoint-initdb.d/test.sql
-echo "Restoring db state from: ${dump_fname} ..."
-pg_restore -c -d perfectfit -U root < ${dump_fname} || { echo "Error occured when trying to restore db from dump: ${dump_fname}" ; exit 1 ; }
-echo "...done"
