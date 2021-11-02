359bfa2a934771eb937bf2ed2be97243efa6dcbb
Robin Richardson <raarichardson@gmail.com>
2021-11-02 11:47:15 +0100

    Update fetch script to fetch/build from rasa-bot and niceday-components repos

--- script/fetch_and_build_all_images.sh
+++ script/fetch_and_build_all_images.sh
@@ -4,21 +4,25 @@ mkdir -p $TEST_DIR
 cd $TEST_DIR || exit 1
 
 # Fetch repos
-git clone https://github.com/PerfectFit-project/virtual-coach-server.git
+git clone https://github.com/PerfectFit-project/rasa-bot.git
+git clone https://github.com/PerfectFit-project/niceday-components.git
 git clone https://github.com/PerfectFit-project/virtual-coach-db.git
 
-cd virtual-coach-server/ || exit 1
+cd ./rasa-bot/ || exit 1
 git pull
 
 # Build Rasa server image
 cd Rasa_Bot/ || exit 1
 docker build . -t e2e-rasa-server || exit 1
-cd ..
 
 # Build Rasa actions image
-cd Rasa_Bot/actions || exit 1
+cd actions/ || exit 1
 docker build . -t e2e-rasa-actions || exit 1
-cd ../..
+
+cd ../../..
+
+cd ./niceday-components || exit 1
+git pull
 
 # Build niceday-broker image
 cd niceday-broker/ || exit 1
@@ -30,10 +34,11 @@ cd ..
 cd niceday-api/ || exit 1
 ./script/bootstrap
 docker build . -t e2e-niceday-api || exit 1
-cd ..
+cd ../..
+
 
 # Build database management image
-cd ../virtual-coach-db/ || exit 1
+cd ./virtual-coach-db || exit 1
 git pull
 docker build . -t e2e-manage-db || exit 1
 cd ..
