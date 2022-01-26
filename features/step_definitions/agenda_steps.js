b50fb0df9ed79706423750ced33907a7eb311dbf
Robin Richardson <raarichardson@gmail.com>
2022-01-26 12:58:22 +0100

    Add planning offer step in js

--- features/step_definitions/agenda_steps.js
+++ features/step_definitions/agenda_steps.js
@@ -1,7 +1,12 @@
 const { Before, Given, When, Then } = require('cucumber')
 require('isomorphic-fetch');
+const assert = require('assert');
 
 const RASA_URL = 'http://localhost:5005';
+const EXPECTED_PLANNING_OFFERS = ['Zal ik de planning in je NiceDay agenda zetten?',
+                                  'Wil je dat ik de planning in je NiceDay agenda zet?'];
+
+let context = {};
 
 Given('rasa bot is up and running', function (callback) {
 
@@ -36,12 +41,22 @@ When('we ask for the agenda', function (callback) {
   })
   .then(response => response.json())
   .then(data => {
+    context.chat_responses = data
     callback();
   })
   .catch((error) => {
     callback(error);
   });
 
-    // context.chat_responses = r.json()
+});
+
+Then('rasa bot offers to add planning to niceday agenda', function (callback) {
+  console.log(context);
+
+  assert(context.hasOwnProperty("chat_responses"));
+  assert(context.chat_responses.length > 0);
+  assert(context.chat_responses.at(-1).hasOwnProperty("text"));
+  assert(EXPECTED_PLANNING_OFFERS.includes(context.chat_responses.at(-1)['text']));
+  callback();
 });
 
