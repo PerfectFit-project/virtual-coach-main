4e9317c6a20035cc88adc86c8af7e2bce35fa813
Robin Richardson <raarichardson@gmail.com>
2022-01-26 12:28:41 +0100

    Add JS implementation of asking for agenda

--- features/step_definitions/agenda_steps.js
+++ features/step_definitions/agenda_steps.js
@@ -1,5 +1,6 @@
 const { Before, Given, When, Then } = require('cucumber')
 const http = require('http')
+require('isomorphic-fetch');
 
 const RASA_URL = 'http://localhost:5005';
 
@@ -8,16 +9,38 @@ Given('rasa bot is up and running', function (callback) {
   http.get(RASA_URL, (res) => {
     const { statusCode } = res;
 
-    let error;
     if (statusCode !== 200) {
-      error = new Error('Request Failed.\n' +
+      const error = new Error('Request Failed.\n' +
                       `Status Code: ${statusCode}`);
       callback(error);
     } else {
       callback();
     }
   });
-
 });
 
+When('we ask for the agenda', function (callback) {
+  const webhookurl = RASA_URL + '/webhooks/rest/webhook';
+
+  const body = {
+      "message": "Kan ik de agenda voor de week krijgen?",
+      "sender": "38527"};
+
+  fetch(webhookurl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(body),
+  })
+  .then(response => response.json())
+  .then(data => {
+    callback();
+  })
+  .catch((error) => {
+    callback(error);
+  });
+
+    // context.chat_responses = r.json()
+});
 
